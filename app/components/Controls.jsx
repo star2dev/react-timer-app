b5f2845599b1d68fb47dfda887c4b381c4411eff
Bruce Galpin <bruce.galpin@arqino.com>
2017-04-02 16:11:09 +0200

    Wired up controls

--- app/components/Controls.jsx
+++ app/components/Controls.jsx
@@ -2,23 +2,29 @@ var React = require('react');
 
 var Controls = React.createClass({
   propTypes: {
-    countdownStatus: React.PropTypes.string.isRquired
+    countdownStatus: React.PropTypes.string.isRquired,
+    onStatusChange: React.PropTypes.func.isRquired
+  },
+  onStatusChange: function (newStatus) {
+    return () => {
+      this.props.onStatusChange(newStatus);
+    };
   },
   render: function () {
     var {countdownStatus} = this.props;
 
     var renderStartStopButton = () => {
       if (countdownStatus === 'started') {
-        return <button className="button secondary">Pause</button>
-      } else if (countdownStatus === 'stopped') {
-        return <button className="button primary">Start</button>
+        return <button className="button secondary" onClick={this.onStatusChange('paused')}>Pause</button>
+      } else if (countdownStatus === 'paused') {
+        return <button className="button primary" onClick={this.onStatusChange('started')}>Start</button>
       }
     };
 
     return (
       <div className="controls">
         {renderStartStopButton()}
-        <button className="button alert hollow">Clear</button>
+        <button className="button alert hollow" onClick={this.onStatusChange('stopped')}>Clear</button>
       </div>
     );
   }
